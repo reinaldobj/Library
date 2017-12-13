5f92c7ee645a234860ded921e4e99a907d74e7e6
Reinaldo Bispo Pereira Junior <reinaldob_j@hotmail.com>
2017-12-12 22:32:55 -0200

    Módulo 3 finalizado

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var jscs = require('gulp-jscs');
+var nodemon = require('gulp-nodemon');
+
+var jsfiles = ['*.js', 'src/**/*.js'];
+
+gulp.task('style', function () {
+    gulp.src(jsfiles)
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish', {
+            verbos: true
+        }))
+        .pipe(jscs());
+});
+
+gulp.task('inject', function () {
+    var wiredep = require('wiredep').stream;
+    var inject = require('gulp-inject');
+
+    var injectSrc = gulp.src([
+        './public/css/*.css',
+        './public/js/*.js'
+    ], {
+        read: false
+    });
+
+    var injectOptions = {
+        ignorePath: '/public'
+    };
+
+    var options = {
+        bowerJson: require('./bower.json'),
+        directory: './public/lib',
+        ignorePath: '../../public'
+    };
+
+    return gulp.src('./src/views/*.html')
+        .pipe(wiredep(options))
+        .pipe(inject(injectSrc, injectOptions))
+        .pipe(gulp.dest('./src/views'));
+});
+
+gulp.task('serve', ['style', 'inject'], function () {
+    var options = {
+        script: 'app.js',
+        delayTime: 1,
+        env: {
+            'PORT': 5000
+        },
+        watch: jsfiles
+    }
+
+    return nodemon(options)
+        .on('restart', function (env) {
+            console.log('Restarting...')
+        });
+});
