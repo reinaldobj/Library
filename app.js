635b68e86adac1ec5c7ea17900aa18f5b425391d
Reinaldo Bispo Pereira Junior <reinaldob_j@hotmail.com>
2017-12-14 23:37:20 -0200

    Módulo 4 finalizado

--- app.js
+++ app.js
@@ -5,10 +5,15 @@ var app = express();
 var port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
-app.use(express.static('src/views'));
+app.set('views', './src/views');
+
+app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-    res.send('Hello World');
+    res.render('index', {
+        title: 'Hello from render',
+        list: ['a', 'b']
+    });
 });
 
 app.get('/books', function (req, res) {
@@ -16,5 +21,5 @@ app.get('/books', function (req, res) {
 });
 
 app.listen(port, function (err) {
-    console.log('running server on port' + port);
+    console.log('running server on port ' + port);
 });
